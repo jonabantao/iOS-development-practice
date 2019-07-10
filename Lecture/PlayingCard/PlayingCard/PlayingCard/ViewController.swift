85c967a58f484bc52c8081abeac3716b0c5b1363
Jonathan Abantao <jonabantao@gmail.com>
2019-07-10 14:29:22 -0700

    Playing Card Lecture

---
+++ Lecture/PlayingCard/PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Jonathan Abantao on 7/5/19.
+//  Copyright © 2019 Jonathan Abantao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
